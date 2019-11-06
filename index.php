b513d5c92421306e3f69ca8b6588d682f5e3023b
josefm09 <josecarlosfloresmoran@hotmail.com>
2019-11-06 16:35:56 -0700

    completado lo que me pidio el guille

--- index.php
+++ index.php
@@ -2,23 +2,34 @@
 <html>
 <body>
 <?php
-include ('../settings/conection.php');
-   
-$sql = 'SELECT reg_hash FROM registro';
-   mysql_select_db('arduino');
-   $retval = mysql_query( $sql, $conn );
-   
-   if(! $retval ) {
-      die('Could not get data: ' . mysql_error());
-   }
-   
-   while($row = mysql_fetch_array($retval, MYSQL_ASSOC)) {
-      echo $row['reg_hash'];
-   }
-   
-   echo "Fetched data successfully\n";
-   
-   mysql_close($conn);
+include ('./settings/conection.php');
+
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    $sql = "SELECT reg_hash FROM registro WHERE id = $id";
+}else {
+    echo "favor de introducir el id correcto";
+    return;
+}
+
+if (!$resultado = $conn->query($sql)) {
+    // ¡Oh, no! La consulta falló. 
+    echo "Lo sentimos, este sitio web está experimentando problemas.";
+
+    // De nuevo, no hacer esto en un sitio público, aunque nosotros mostraremos
+    // cómo obtener información del error
+    echo "Error: La ejecución de la consulta falló debido a: \n";
+    echo "Query: " . $sql . "\n";
+    echo "Errno: " . $conn->errno . "\n";
+    echo "Error: " . $conn->error . "\n";
+    exit;
+}
+$data = $resultado->fetch_assoc();
+
+echo $data['reg_hash'];
+
+$conn->close();
 ?> 
 </body>
 </html>
--- settings/conection.php
+++ settings/conection.php
@@ -2,13 +2,15 @@
 $servername = "localhost";
 $username = "root";
 $password = "jose1234";
+$database = "arduino";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+//echo "conectado";
 ?>
